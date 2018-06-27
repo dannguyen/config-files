f3f5df79675d1089c951cf26f37a17d7c3bb4fc9
Dan Nguyen <dansonguyen@gmail.com>
2018-06-27 14:51:15 -0700

    Create lscolors.sh

---
+++ lscolors.sh
@@ -0,0 +1,16 @@
+# https://www.cyberciti.biz/tips/freebsd-how-to-enable-colorized-ls-output.html
+export CLICOLOR=1
+LSCOLORS=''
+LSCOLORS=${LSCOLORS}Gx # directory
+LSCOLORS=${LSCOLORS}fa # sym link
+LSCOLORS=${LSCOLORS}Da # socket
+LSCOLORS=${LSCOLORS}ca # pipe
+LSCOLORS=${LSCOLORS}Cx # executable
+LSCOLORS=${LSCOLORS}fe # block special
+LSCOLORS=${LSCOLORS}ef # character special
+LSCOLORS=${LSCOLORS}ab # executable with setuid bit set
+LSCOLORS=${LSCOLORS}ag # executable with setgid bit set
+LSCOLORS=${LSCOLORS}ac # directory writable to others, with sticky bit
+LSCOLORS=${LSCOLORS}ad # directory writable to others, without sticky bit
+
+export LSCOLORS
