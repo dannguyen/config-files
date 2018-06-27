8cf41f96e43b86b411d47dd3cca5cd1a14947521
Dan Nguyen <dansonguyen@gmail.com>
2018-06-27 15:25:53 -0700

    Create ps1prompts.sh

---
+++ ps1prompts.sh
@@ -0,0 +1,19 @@
+# ᕕ( ᐛ )ᕗ
+XRESET='\[\033[00m\]'
+PATH_ACCENT='\[\033[0;36m\]'
+# http://xahlee.info/comp/unicode_matching_brackets.html
+# XBRAX_L="${XACCENT}⧼${XRESET}"
+# XBRAX_R="${XACCENT}⧽${XRESET}"
+XWPATH="\[\033[0;33m\] \w${XRESET}"
+PROMPT_PART="${PATH_ACCENT}\W${XRESET} \[\033[1;37m\]\$${XRESET}"
+
+PROMPT_MONSTER="\[\033[0;35m\](╯°□°)╯\[\033[00m\] \[\033[1;31m\]\w\[\033[00m\] \[\033[1;37m\]$\[\033[00m\] "
+PROMPT_GHOST="༼つ\[\033[31m\](\[\033[5m\]♥\[\033[0m\033[31m\])\[\033[0m\]_■༽つ \W $ "
+# https://www.key-shortcut.com/en/writing-systems/35-symbols/arrows/
+PROMPT_HADOUKEN="⤷ 👊 ${PROMPT_PART} "
+PROMPT_SHORYUKEN="⇒ ⇓ ⇘ 👊 ${PROMPT_PART} "
+PROMPT_KONAMI="[⇧ ⇧ ⇩ ⇩ ⇦ ⇨ ⇦ ⇨ Ⓑ Ⓐ ] ${PROMPT_PART} "
+# https://en.wikipedia.org/wiki/Chess_symbols_in_Unicode
+PROMPT_CHESS="\[\033[0;37m\]♘ \[\033[0;35m\]♕ \[\033[0;37m\]♘${XRESET}  ${PROMPT_PART} "
+
+export PS1=$PROMPT_CHESS
